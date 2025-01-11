4cfcbd4e91faf79123ac2798800ac4321ef40143
OllisDev <ikermagro@outlook.es>
2025-01-11 13:10:16 +0100

    Sesion1101

--- app/src/main/java/es/ejemplo/android/drawtasks/MainActivity.kt
+++ app/src/main/java/es/ejemplo/android/drawtasks/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_tasks, R.id.nav_createTasks, R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_tasks, R.id.nav_createTasks
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
